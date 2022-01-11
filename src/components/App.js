8caf8eff4da21ff4cd2625d414c7d29f642b62e6
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2022-01-11 13:58:15 -0700

    deliverable 3

--- src/components/App.js
+++ src/components/App.js
@@ -18,13 +18,29 @@ function App() {
     setFeatPup(pup)
   }
 
-  console.log(featPup)
+  const onGoodBad = () => {
+    
+    setFeatPup({...featPup, isGoodDog: !featPup.isGoodDog})
+    
+    const body = {
+      ...featPup,
+      isGoodDog: !featPup.isGoodDog
+    }
+    fetch(`http://localhost:3001/pups/${featPup.id}`,{
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(body)
+    })
+  }
+
 
   useEffect(getPups, [])
 
   let pupSpans = pups.map(p => <span key={p.id} onClick={onPupClick}>{p.name}</span>)
 
-  let pupInfo = featPup ? <><img src={featPup.image} alt={featPup.className} /><h2>{featPup.name}</h2> <button>{featPup.isGoodDog ? "Good Dog!" : "Bad Dog!"}</button></> : null
+  let pupInfo = featPup ? <div id={featPup.id}><img src={featPup.image} alt={featPup.className} /><h2>{featPup.name}</h2> <button onClick={onGoodBad}>{featPup.isGoodDog ? "Good Dog!" : "Bad Dog!"}</button></div> : null
 
   return (
     <div className="App">
